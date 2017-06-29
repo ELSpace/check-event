4a359dd4351ef1edd97a97cc9a079fb99dd7d77b
Amenallah <a.az0000@yahoo.com>
2017-06-29 15:41:06 +0100

    split metronic index into seperate folders

--- app/metronic/index.js
+++
@@ -1,9 +0,0 @@
-import Header from './layout/header/Header';
-import Sidebar from './layout/sidebar/Sidebar'; 
-import Content from './layout/content/Content'; 
-
-export {
-  Header, 
-  Sidebar, 
-  Content
-};
---
+++ app/metronic/layout/index.js
@@ -0,0 +1,9 @@
+import Header from './header/Header';
+import Sidebar from './sidebar/Sidebar'; 
+import Content from './content/Content'; 
+
+export {
+  Header, 
+  Sidebar, 
+  Content
+};
