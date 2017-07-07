13a73a49f917074a2a6749243fbee48239a9f7a5
Amenallah <a.az0000@yahoo.com>
2017-07-07 15:30:49 +0100

    include pace styles in assets

--- app/assets/index.js
+++ app/assets/index.js
@@ -3,10 +3,13 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'simple-line-icons/css/simple-line-icons.css';
 import 'bootstrap-switch/dist/css/bootstrap3/bootstrap-switch.css';
 import 'fullcalendar/dist/fullcalendar.css'; 
+import 'toastr/build/toastr.css';
  
 import '../metronic/theme/css/components-md.css';
 import '../metronic/theme/css/layout.css'; 
 import '../metronic/theme/css/blue.css';
+
+import './pace.css';
 
 // JS  
 require('bootstrap/dist/js/bootstrap.js'); 
@@ -16,4 +19,5 @@ require('jquery-slimscroll/jquery.slimscroll.js');
 require('block-ui/jquery.blockUI.js');
 require('js-cookie/src/js.cookie.js');
 require('../metronic/theme/js/layout.js');
- 
+
+//window.Pace = require('pace/pace.js');
