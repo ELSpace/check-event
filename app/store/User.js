7dfbeaf0a4c5378caa1296f1285c5277c9851b70
Amenallah <a.az0000@yahoo.com>
2017-07-07 15:31:08 +0100

    seperate login and profile fetching into two actions

--- app/store/User.js
+++ app/store/User.js
@@ -4,22 +4,37 @@ import feathersClient from '../services/feathers';
 
 export default class User {
 
-  @observable fullname;
-  @observable jwt;
+  @observable user;
+  @observable token;
+  @observable isLoggedIn = false;
 
-  @action.bound login = async () => {
-    try {
-      const response = await feathersClient.authenticate({
-        strategy: 'local',
-        email: 'mehrez@hrouz.com',
-        password: 'hrouz'
-      });
-      const payload = await feathersClient.passport.verifyJWT(response.accessToken);
-      const user = await feathersClient.service('users').get(payload.userId);
-      console.log(user);
-    } catch(e) {
-      
-    }
+  @action.bound profile = async () => {
+    this.token = await feathersClient.passport.getJWT();
+    
+    if (!token) throw new Error('Not logged in');    
+    
+    if (this.user = feathersClient.set('user')) return;
+
+    return await this.get(token);
+  }
+
+  @action.bound login = async ({email, password}) => {
+    const response = await feathersClient.authenticate({
+      strategy: 'local',
+      email,
+      password
+    });
+
+    return await this.get(response.accessToken);
+  }
+
+  @action.bound get = async token => {
+    const payload = await feathersClient.passport.verifyJWT(token);
+    const user = await feathersClient.service('users').get(payload.userId);
+    
+    feathersClient.set('user', user);
+    this.user = user;
+    this.isLoggedIn = true;
   }
 
 }
