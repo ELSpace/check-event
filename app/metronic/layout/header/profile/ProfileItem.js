c8d1de5aa245c971ec119cb06788a2f8f4545492
Amenallah <a.az0000@yahoo.com>
2017-07-08 17:16:28 +0100

    fix links in profile items

--- app/metronic/layout/header/profile/ProfileItem.js
+++ app/metronic/layout/header/profile/ProfileItem.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const ProfileItem = ({icon, title, badge, badgeColor, url, onClick}) => (
   <li>
-    <a href={url} onClick={onClick}>
+    <Link to={url} onClick={onClick}>
       <i className={icon}></i> {title}
       {badge && <span className={`badge badge-${badgeColor}`} > {badge} </span>}
-    </a>
+    </Link>
   </li>
 );
 
