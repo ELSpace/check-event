b194ef8237b9120f156c27505a36cdd06e4fdb94
medamine95 <benbelgacem.mohamed@live.com>
2017-07-08 18:51:32 +0100

    add new Route

---
+++ app/components/forms/new/New.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Content } from '../../metronic/layout';
+
+const New = () => (
+  <Content.Item title="New Form" description="new form">
+
+
+
+
+
+
+
+
+
+
+  </Content.Item>
+);
+
+
+export default New;
