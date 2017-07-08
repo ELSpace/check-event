028fab26077a296f8d111feaa790dc3ec26f6f1d
Amenallah <a.az0000@yahoo.com>
2017-07-08 17:16:19 +0100

    create logout component

---
+++ app/components/user/logout/Logout.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import { observer, inject } from 'mobx-react'
+import Loadable from 'react-loading-overlay';
+import toastr from 'toastr';
+
+@withRouter
+@inject('store')
+@observer
+class Logout extends React.Component {
+  componentDidMount() {
+    this.props.store.user.logout()
+      .then(() => {
+        this.props.history.push('/login');
+      })
+      .catch(() => {
+        toastr.error('There was a problem logging you out');
+      })
+    
+  }
+
+  render() {
+    return <Loadable
+      active={true}
+      spinner
+      text={'Please John...please John please come hooowem'}
+    >
+      <div style={{minHeight: '100%'}}></div>;
+    </Loadable>
+  }
+}
+
+export default Logout;
