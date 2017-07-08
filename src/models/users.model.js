e2cfa122c0f5c85f4ba0ab4a9a66a3162687240e
Amenallah <a.az0000@yahoo.com>
2017-07-08 19:43:16 +0100

    update user model

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -8,7 +8,11 @@ module.exports = function (app) {
   
     email: { type: String, required:true, unique: true},
     password: { type: String, required: true },
-  
+    fullname: { type: String, required: true },
+    address: { type: String, required: true },
+    city: { type: String, required: true },
+    country: { type: String, required: true },
+
   
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
