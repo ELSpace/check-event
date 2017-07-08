b11e735f70c9b59b24baf396c8869c2f74a786f3
Amenallah <a.az0000@yahoo.com>
2017-07-08 16:31:27 +0100

    remove unnaicessary profile item

--- app/common/profileItems.js
+++ app/common/profileItems.js
@@ -1,16 +1,8 @@
 export default [
   {
     id: 1,
-    icon: 'icon-envelope-open',
-    title: 'My Inbox',
-    badge: 3,
-    badgeColor: 'danger',
-    url: '/'
-  },
-  {
-    id: 2,
     icon: 'icon-key',
     title: 'Logout',
-    url: '/'
+    url: '/logout'
   },
 ];
