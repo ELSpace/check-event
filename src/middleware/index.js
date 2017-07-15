4899ef0307365c186de60fea3025671aad2a1603
Amenallah <a.az0000@yahoo.com>
2017-07-15 23:51:50 +0100

    fix server paths

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,5 +1,6 @@
 const handler = require('feathers-errors/handler');
 const notFound = require('feathers-errors/not-found');
+const path = require('path');
 
 module.exports = function () {
   // Add your custom middleware here. Remember, that
@@ -7,10 +8,15 @@ module.exports = function () {
   // the error handler have to go last.
   const app = this;
 
-  app.get('*', (req, res) => {
-    res.sendFile('../../public/index.html');
+   app.use((req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, '../../public/index.html')
+    )
   });
+
 
   app.use(notFound());
   app.use(handler());
+
+  
 };
