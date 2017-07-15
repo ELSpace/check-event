a0a99ba0a44e9e174ad2bd0608ed304d7c98dede
Amenallah <a.az0000@yahoo.com>
2017-07-15 14:48:28 +0100

    render form

--- app/components/forms/display/Display.js
+++ app/components/forms/display/Display.js
@@ -1,22 +1,42 @@
 import React, { Component } from 'react';
 import { Content } from '../../../metronic/layout';
-let formR;
+import { observer, inject } from 'mobx-react';
 
+@inject('store')
+@observer
 class Display extends Component {
 
-    componentDidMount() {  
-    formR = $('#render').formRender({
-        dataType: 'xml',
-    formData: formR.value,
-  });   
-    
+  componentDidMount() {
+    window.$('body').css('background', '#ffffff');
+    const param = this.props.match.params.form;
+    this.props.store.form.getSingle(param)
+      .then(fields => {
+        console.log(fields);
+        window.$('#render').formRender({
+          dataType: 'json',
+          formData: fields
+        });
+      });
   }
 
-render() {
-  return(
-        <Content.Item title="Display Form" description="Display a Form ">
+  render() {
+    return <div className="container">
+      <div className="row">
+        <div className="col-md-6 col-md-offset-3">
+          <h2><b>{this.props.match.params.form.replace('-', ' ')}</b></h2>
+          <hr />
           <div id="render"></div>
-        </Content.Item> );
-}}
+          <hr />
+          <button className="pull-right btn btn-success">Submit</button>
+          <br />
+          <br />
+          <br />
+        </div>
+      </div>
+    </div>
+          
+  }
+
+}
 
 export default Display;
