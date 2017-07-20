26566a62b6a74b505262feca9923886815c604a6
medamine95 <benbelgacem.mohamed@live.com>
2017-07-20 02:49:03 +0100

    Implmenting The New Register Form

--- app/components/user/register/Form.js
+++ app/components/user/register/Form.js
@@ -1,93 +1,90 @@
 import React from 'react';
-import classNames from 'classnames';
+import classNameNames from 'classnames';
 import {Link} from 'react-router-dom';
-import { Button } from '../../../metronic/lib/ui';
 import { observer } from 'mobx-react';
-
-
+import { Button, Form, FormGroup, Label, Input, FormFeedback } from 'reactstrap';
 
 export default observer(({form}) => (
-    <form onSubmit={form.onSubmit} className="register-form" noValidate="novalidate" style={{display: 'block'}}>
-    <h3 className="font-green">REGISTER</h3>
-    <p className="hint"> Enter your Association/Company details below: </p>
 
-    <div className={"form-group " + (form.$('fullname').hasError ? 'has-error' : '')}>
-        <label className="control-label visible-ie8 visible-ie9" htmlFor={form.$('fullname').id}>
-          {form.$('fullname').label}
-        </label>
-        <input className="form-control placeholder-no-fix"
-        type="text"
-          {...form.$('fullname').bind()}/>
-        <span className="help-block"> {form.$('fullname').error} </span>
-         </div>
-   
-       <div className={"form-group " + (form.$('address').hasError ? 'has-error' : '')}>
-        <label className="control-label visible-ie8 visible-ie9" htmlFor={form.$('address').id}>
-          {form.$('address').label}
-          </label>
-        <input className="form-control placeholder-no-fix"
-         type="text"
-           {...form.$('address').bind()} />
-          <span className="help-block"> {form.$('address').error} </span>
-           </div>
-         
-    <div className={"form-group " + (form.$('city').hasError ? 'has-error' : '')}>
-        <label className="control-label visible-ie8 visible-ie9" htmlFor={form.$('city').id}>
-         {form.$('city').label}
-        </label>
-        <input className="form-control placeholder-no-fix" type="text" {...form.$('city').bind()} /> 
-        <span className="help-block"> {form.$('city').error} </span>
-        </div>
-     
-    <div className={"form-group " + (form.$('country').hasError ? 'has-error' : '')}>
-        <label className="control-label visible-ie8 visible-ie9" htmlFor={form.$('country').id}>
-         {form.$('country').label}
-         </label>
-        <select name="country" className="form-control" {...form.$('country').bind()}>
-          <option value>Country</option>
-          <option value="FR">France</option>               
-          <option value="TN">Tunisia</option>               
-          <option value="GB">United Kingdom</option>
-          <option value="US">United States</option>              
-        </select>
-        <span className="help-block"> {form.$('country').error} </span>     
-     </div> 
-          
-      <p className="hint"> Enter your Association/Company details below: </p>
-      <div className={"form-group " + (form.$('email').hasError ? 'has-error' : '')}>
-        <label className="control-label visible-ie8 visible-ie9" htmlFor={form.$('email').id}>
-        {form.$('email').label}
-        </label>
-        <input className="form-control placeholder-no-fix" type="text" {...form.$('email').bind()} />
-        <span className="help-block"> {form.$('email').error} </span>
-         </div>
+<div data-reactroot className="app flex-row align-items-center">
+  <div className="container">
+    <div className="row justify-content-center">
+      <div className="col-md-6">
+        <div className="card mx-2">
+          <div className="card-block p-2">
+            <h1>Register</h1>
+            <p className="text-muted">Create your account</p>
+          <form onSubmit={form.onSubmit}>
+            <div className={"input-group mb-1" + (form.$('fullname').hasError ? 'has-error' : '')}>
+              
+                <label className="control-label visible-ie8 visible-ie9" htmlFor={form.$('fullname').id}>
+                </label>
+                <span className="input-group-addon">
+                <i className="fa fa-user-circle" /></span>
+                <input type="text" className="form-control"  {...form.$('fullname').bind()}/>
+                <span className="help-block"> {form.$('fullname').error} </span>
+                </div>
 
-      <div className={"form-group " + (form.$('password').hasError ? 'has-error' : '')}>
-        <label className="control-label visible-ie8 visible-ie9" htmlFor={form.$('password').id}>
-         {form.$('password').label}
-        </label>
-        <input className="form-control placeholder-no-fix" type="password" {...form.$('password').bind({ type: 'password' })} />
-         <span className="help-block"> {form.$('password').error} </span>
-         </div>
-         
-      <div className="form-group margin-top-20 margin-bottom-20">
-        <label className="mt-checkbox mt-checkbox-outline">
-          <input type="checkbox" name="tnc" /> I agree to the
-          <a href="javascript:;">Terms of Service </a> &amp;
-          <a href="javascript:;">Privacy Policy </a>
-          <span />
-        </label>
-        <div id="register_tnc_error"> </div>
-      </div> 
-      <div className="form-actions">
-        <Link to='/login'>
-        <button type="button" id="register-back-btn" className="btn green btn-outline">Back</button>
-        </Link>
-        <button type="submit" className="btn btn-success uppercase pull-right">Submit</button>
-      </div>
-    <p>{form.error}</p>
-  </form>
-));
+                <div className={"input-group mb-1" + (form.$('address').hasError ? 'has-error' : '')}>
+                 <span className="input-group-addon">
+                  <i className="fa fa-map-marker"/> </span>             
+                 <label className="control-label visible-ie8 visible-ie9" htmlFor={form.$('address').id}>
+                 </label>
+                  <input type="text" className="form-control" placeholder="Adress" {...form.$('address').bind()} />
+                  <span className="help-block"> {form.$('address').error} </span>
+                  </div>
+
+                  <div className={"input-group mb-1" + + (form.$('phone').hasError ? 'has-error' : '')}>
+                 <span className="input-group-addon">
+                   <i className="fa fa-phone-square"/>
+                 </span>
+                  <label className="control-label visible-ie8 visible-ie9" htmlFor={form.$('phone').id}>
+                   </label>
+                  <input type="number" className="form-control" placeholder="Phone Number" {...form.$('phone').bind()} />
+                  <span className="help-block"> {form.$('phone').error} </span>
+                  </div>
+               
+                <div className={"input-group mb-1"  + (form.$('email').hasError ? 'has-error' : '')}>
+                  <span className="input-group-addon">
+                    <i className="fa fa-envelope"/>
+                  </span>
+                  <label className="control-label visible-ie8 visible-ie9" htmlFor={form.$('email').id}>
+                  </label>
+                  <input type="text" className="form-control" placeholder="Email" {...form.$('email').bind()} />
+                  <span className="help-block"> {form.$('email').error} </span>
+                  </div>                 
+                  <div className={"input-group mb-1"+(form.$('password').hasError ? 'has-error' : '')}>
+                   <span className="input-group-addon">
+                    <i className="fa fa-key" /></span>
+                  <label className="control-label visible-ie8 visible-ie9" htmlFor={form.$('password').id}>
+                  </label>                  
+                      <input type="password" className="form-control" placeholder="Password" {...form.$('password').bind({ type: 'password' })} />
+                       <span className="help-block"> {form.$('password').error} </span>
+                      </div>
+                        <button type="submit" onClick={form.onSubmit} className="btn btn-block btn-success">Create Account</button>
+                        <p>{form.error}</p>
+                       </form>
+                        </div>
+              <div className="card-footer p-2">
+              <div className="row">
+                <div className="col-6">
+                  <button className="btn btn-block btn-facebook" type="button">
+                     <span>facebook</span>
+                   </button>
+                </div>
+                <div className="col-6">
+                <button className="btn btn-block btn-twitter" type="button">
+                      <span> twitter </span>
+                </button>
+                </div>
+                  </div>
+                    </div>
+                      </div>
+                        </div>
+                          </div>
+                            </div>
+                              </div>
+ ));
 
 
 
