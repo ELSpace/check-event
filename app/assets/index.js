23bc46f1dead98d65507508853f5f818399965b1
medamine95 <benbelgacem.mohamed@live.com>
2017-07-21 17:13:46 +0100

    importing jquery ui + formbuilder +formrender

--- app/assets/index.js
+++ app/assets/index.js
@@ -5,7 +5,11 @@ import 'toastr/build/toastr.css';
 import './css/simple-line-icons.css';
 import './css/font-awesome.css';
 import './css/style.css'; 
+import 'jquery-ui-dist/jquery-ui.min.css';
+import 'jquery-ui-dist/jquery-ui.theme.min.css';
+import 'jquery-ui-dist/jquery-ui.structure.min.css';
+   
 
 // JS  
-//require('formBuilder');
-//require('formBuilder/dist/form-render.min'); // contains babel-polyfill
+require('formBuilder');
+require('formBuilder/dist/form-render.min'); // contains babel-polyfill
