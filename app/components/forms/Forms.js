f2c1780574155c5c489acc22607ff26452d05853
Amenallah <a.az0000@yahoo.com>
2017-07-22 22:06:49 +0100

    seperate forms table into seperate component

--- app/components/forms/Forms.js
+++ app/components/forms/Forms.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { observer, inject } from 'mobx-react';
+import { Card, CardTitle, CardSubtitle, CardBlock } from 'reactstrap';
+
+import FormsTable from './FormsTable';
 
 @inject('store')
 @observer
@@ -12,53 +15,22 @@ export default class Forms extends React.Component {
 
   render() {
     return (
-        <div className="container-fluid">
-          <div className="actions">
-            <Link to='/forms/new'> 
-              <button className='btn btn-primary'>Create a New Form</button> 
-            </Link>
-          </div>
-          <hr/>
-          
-        <div className="card">
-          <div className="card-header">
-            <i className="fa fa-align-justify" />
-            {/* react-text: 10834 */} Striped Table{/* /react-text */}
+      <div className="container-fluid">
+        <Card>
+          <CardBlock>
+            <CardTitle>My Forms</CardTitle>
+            <CardSubtitle>form management</CardSubtitle>
+            <hr/>
+            <div className="actions">
+              <Link to='/forms/new'> 
+                <button className='btn btn-primary'>Create a New Form</button> 
+              </Link>
             </div>
-            <div className="card-block">
-              <table className="table table-striped table-responsive table-bordered">
-                <thead>
-                  <tr>
-                    <th>Form Name</th>
-                    <th>Entries	</th>
-                    <th>Date</th>
-                    </tr>
-                    </thead>
-                    <tbody>                
-                    {this.props.store.form.forms.map(form => 
-                      <tr key={form._id} role="row" className="odd">
-                        <td className="sorting_1"><Link to={'/display/'+form.name} target='_blank'>
-                        {form.name} 
-                        </Link>
-                        </td>
-                        <td>{form.entries.length}</td>
-                        <td>{form.createdAt}</td>
-                      </tr>  
-                        )}
-                        </tbody>
-                        
-                        </table>
-                        
-                        <ul className="pagination">
-                          <li className="page-item"><a className="page-link" href="#">Prev</a></li>
-                          <li className="page-item active"><a className="page-link" href="#">1</a>
-                          </li><li className="page-item"><a className="page-link" href="#">2</a>
-                          </li><li className="page-item"><a className="page-link" href="#">3</a></li>
-                          <li className="page-item"><a className="page-link" href="#">4</a></li><li className="page-item">
-                            <a className="page-link" href="#">Next</a></li></ul></div></div>                        
-
-         </div> 
+            <br />
+            <FormsTable forms={this.props.store.form.forms}></FormsTable>
+          </CardBlock>
+        </Card>
+      </div>
     );
   }
 }
-
---
+++ app/components/forms/FormsTable.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const FormsTable = ({forms}) => 
+  <div>
+    <table className="table table-responsive table-bordered">
+    <thead>
+        <tr>
+          <th>Form Name</th>
+          <th>Entries	</th>
+          <th>Date</th>
+        </tr>
+    </thead>
+    <tbody>
+        {forms.map(form => 
+        <tr key={form._id} role="row" className="odd">
+          <td>
+              <Link to={'/display/'+form.name} target='_blank'>
+              {form.name} 
+              </Link>
+          </td>
+          <td>{form.entries.length}</td>
+          <td>{form.createdAt}</td>
+        </tr>
+        )}
+    </tbody>
+    </table>
+    <ul className="pagination">
+    <li className="page-item">
+        <a className="page-link" href="#">Prev</a>
+    </li>
+    <li className="page-item active">
+        <a className="page-link" href="#">1</a>
+    </li>
+    <li className="page-item">
+        <a className="page-link" href="#">2</a>
+    </li>
+    <li className="page-item">
+        <a className="page-link" href="#">3</a>
+    </li>
+    <li className="page-item">
+        <a className="page-link" href="#">4</a>
+    </li>
+    <li className="page-item">
+        <a className="page-link" href="#">Next</a>
+    </li>
+    </ul>
+  </div>;
+
+export default FormsTable;
