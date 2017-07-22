7a47e64803f5e0a740f90173ddc533037a8a96b5
Amenallah <a.az0000@yahoo.com>
2017-07-22 23:12:13 +0100

    add settings router for account and page

--- app/components/settings/Settings.js
+++ app/components/settings/Settings.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import { Card, CardTitle, CardSubtitle, CardBlock } from 'reactstrap';
+import { Route, Link, Switch, Redirect } from 'react-router-dom';
+
+import './style.css';
 
 const Settings = () => 
-  <div className="container-fluid">
+  <div className="container-fluid settings">
     <Card>
       <CardBlock>
         <CardTitle>Settings</CardTitle>           
-        <hr/>        
+        <hr />        
         <br />
         <div className="row">
-          <div className="col-md-4">
-            <nav className="nav flex-column">
-              <a className="nav-link active" href="#">Active</a>
-              <a className="nav-link" href="#">Link</a>
-              <a className="nav-link" href="#">Link</a>
-              <a className="nav-link disabled" href="#">Disabled</a>
+          <div className="col-md-2">
+            <nav className="nav">
+              <Link className="nav-link flex-column active" to="/settings/account">Account</Link>
+              <Link className="nav-link" to="/settings/page">Page</Link>
             </nav>
           </div>
-          <div className="col-md-8">
-            <h2>huzzaj</h2>
+          <div className="col-md-10">             
+            <Switch>
+              <Route path="/settings/page" component={() => <h1>Page settings</h1>}></Route>
+              <Route path="/settings/account" component={() => <h1>Account settings</h1>}></Route>            
+              <Redirect from="/" to="/settings/account" />
+            </Switch>
           </div>
         </div>
       </CardBlock>
