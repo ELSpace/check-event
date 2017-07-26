bae6a70285d945e23b3a27f2e0e21120066ca373
Amenallah <a.az0000@yahoo.com>
2017-07-26 16:44:41 +0100

    build

--- app/common/index.js
+++ app/common/index.js
@@ -2,6 +2,17 @@ import PrivateRoute from './PrivateRoute';
 import actions from './actions';
 import profileItems from './profileItems';
 import routes from './routes';
+import toastr from 'toastr';
+
+toastr.options = {
+  "debug": false,
+  "positionClass": "toast-bottom-right",
+  "onclick": null,
+  "fadeIn": 300,
+  "fadeOut": 1000,
+  "timeOut": 5000,
+  "extendedTimeOut": 1000
+}
 
 export {
   PrivateRoute,
