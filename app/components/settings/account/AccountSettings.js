9bc9903e5ba7f462c6519d555be31dfab40a19f3
Amenallah <a.az0000@yahoo.com>
2017-07-27 01:12:48 +0100

    fix immodifiable accounts settiings inputs

--- app/components/settings/account/AccountSettings.js
+++ app/components/settings/account/AccountSettings.js
@@ -2,15 +2,24 @@ import React, { Component } from 'react';
 import { FormGroup, Label, Input } from 'reactstrap';
 import { observer, inject } from 'mobx-react';
 
-@inject('store')
+@inject(allStores => ({user: allStores.store.user}))
 @observer
 class AccountSettings extends Component {
+
   componentWillMount() {
-    this.props.store.user.profile();
+    this.props.user.profile();
+  }
+
+  onChange(event) {
+    const value = event.target.value;
+    const name = event.target.name;
+    this.props.user.user[name] = value;
   }
 
   render() {
-    const user = this.props.store.user.user || {};
+    const user = this.props.user.user || {};
+
+    this.onChange = this.onChange.bind(this);
     
     return (
       <div>
@@ -20,19 +29,19 @@ class AccountSettings extends Component {
         <form>
           <FormGroup>
             <Label>Email</Label>
-            <Input type="email" name="email" value={user.email} />
+            <Input type="email" name="email" value={user.email} onChange={this.onChange}/>
           </FormGroup> <br />
           <FormGroup>
             <Label>Fullname</Label>
-            <Input type="text" name="fullname" value={user.fullname} />
+            <Input type="text" name="fullname" value={user.fullname} onChange={this.onChange} />
           </FormGroup> <br />
           <FormGroup>
             <Label>Address</Label>
-            <Input type="text" name="city" value={user.address} />
+            <Input type="text" name="address" value={user.address} onChange={this.onChange} />
           </FormGroup> <br />
           <FormGroup>
             <Label>City</Label>
-            <Input type="text" name="city" value={user.city} />
+            <Input type="text" name="city" value={user.city} onChange={this.onChange} />
           </FormGroup>
           <FormGroup> <br />
             <Label>Company/Organisation Name</Label>
