c0b84678b083444c3493a9d7f35bff51cf6b992c
Amenallah <a.az0000@yahoo.com>
2017-08-07 13:28:42 +0100

    seperate api and public files

--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ const feathers = require('feathers');
 const logger = require('winston');
 const app = require('./app');
 const port = app.get('port');
-const server = 
-  new feathers().use('/api/v1', app).listen(port); 
+const path = require('path');
+const configuration = require('feathers-configuration');
+
+const api = new feathers().use('/api/v1', app).listen(port); 
+const public = new feathers()
+  .configure(configuration(path.join(__dirname, '..')))
+  .use(feathers.static(app.get('public')))
+  .listen(5000);
 
 process.on('unhandledRejection', (reason, p) =>
   logger.error('Unhandled Rejection at: Promise ', p, reason)
 );
 
-server.on('listening', () =>
+api.on('listening', () =>
   logger.info(`Feathers application started on ${app.get('host')}:${port}`)
 );
