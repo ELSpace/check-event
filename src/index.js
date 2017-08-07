6c13534fd81a9112fca56a3af1c103f0e5aebab6
Amenallah <a.az0000@yahoo.com>
2017-08-07 13:47:20 +0100

    fix server api and public issue

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ const feathers = require('feathers');
 const logger = require('winston');
 const app = require('./app');
 const port = app.get('port');
-const path = require('path');
 const configuration = require('feathers-configuration');
+const path = require('path');
 
-const api = new feathers().use('/api/v1', app).listen(port); 
-const public = new feathers()
+const api = new feathers()
   .configure(configuration(path.join(__dirname, '..')))
   .use(feathers.static(app.get('public')))
-  .listen(5000);
+  .use('/api/v1', app)
+  .listen(port); 
 
 process.on('unhandledRejection', (reason, p) =>
   logger.error('Unhandled Rejection at: Promise ', p, reason)
